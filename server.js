86ecc903d7e704c13894fab15afe940f9c61ba9a
Gyunseo Lee <rbstj0311@naver.com>
2022-02-05 23:55:11 +0900

    modified comment of server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); // 설치한 프레임워크 사용하겠다
-const app = express(); //
+const app = express(); // express 객체를 생성한다.
 
 app.listen(8081, () => {
     console.log('listening on 8081');
